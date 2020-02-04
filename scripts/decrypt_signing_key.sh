e1d3ff8464bc698fe51a0d9f211d62da3a4d0d7d
Devin Sit <devin@devinsit.com>
2020-02-03 23:57:50 -0500

    TSR-17 Created the key for signing release builds; encrypted it (and its properties file) for repo storage.

---
+++ scripts/decrypt_signing_key.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Decrypt the actual keystore used for signing release APKs
+gcloud kms decrypt --plaintext-file=./android/app/transitr-release-key.keystore --ciphertext-file=./android/app/transitr-release-key.keystore.encrypted --location=us-east1 --keyring=transitr-key-ring --key=transitr-keystore-key
+
+# Decrypt the properties file with the keystore location and passwords
+gcloud kms decrypt --plaintext-file=./android/keystore.properties --ciphertext-file=./android/keystore.properties.encrypted --location=us-east1 --keyring=transitr-key-ring --key=transitr-keystore-key
